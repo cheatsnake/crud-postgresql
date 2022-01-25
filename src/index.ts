eaf97f727dfca40015c8c16591bee2719aab8d87
Yury <72691412+cheatsnake@users.noreply.github.com>
2022-01-25 21:07:56 +0300

    Create server

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import "dotenv/config";
+import express, { Application, Request, Response } from "express";
+import noteRouter from "routers/note.router";
+import personRouter from "routers/person.router";
+
+const PORT = process.env.PORT || 3000;
+const app: Application = express();
+
+app.use(express.json());
+app.use("/api", noteRouter);
+app.use("/api", personRouter);
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello, World!");
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
